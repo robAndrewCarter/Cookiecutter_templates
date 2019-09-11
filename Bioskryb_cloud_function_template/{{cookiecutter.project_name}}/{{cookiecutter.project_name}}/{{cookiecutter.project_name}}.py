cf7997206308e2b072706f4612acd519f6b415e5
robAndrewCarter <r.andrew.carter@gmail.com>
2019-09-10 21:06:09 -0400

    fixed function def

--- Bioskryb_cloud_function_template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}.py
+++ Bioskryb_cloud_function_template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}.py
@@ -8,5 +8,5 @@ log_handler.setLevel(logging.INFO)
 logger.addHandler(log_handler)
 
 
-def {{cookiecutter.project_name}}:
+def {{cookiecutter.project_name}}(event, context):
     logger.info("Cloud Function : {{cookiecutter.project_name}}.py, Version {}".format(__VERSION__))
